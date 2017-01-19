9388d7b304d39f48393bbff0bed1b5630fd270ed
Marian L. Schmidt <marschmi@umich.edu>
2017-01-19 16:14:05 -0500

    moved files around to help organize repository

---
+++ code/set_colors.R
@@ -0,0 +1,70 @@
+## Purpose:  Set the global colors based on important factors
+## Author: Marian L. Schmidt 
+
+
+# Set global colors for the different taxonomic phyla
+phylum_colors <- c(
+  Acidobacteria = "navy", 
+  Actinobacteria = "darkslategray2", 
+  Alphaproteobacteria = "tomato", 
+  Aminicenantes = "cornflowerblue",
+  Armatimonadetes = "wheat", 
+  Bacteria_unclassified = "#508578", 
+  Bacteroidetes = "gold", 
+  Betaproteobacteria = "plum1", 
+  "Candidate_division_OP3" = "slategray3",
+  Chlamydiae = "#A20E42",
+  Chlorobi="magenta", 
+  Chloroflexi="black", 
+  Cyanobacteria = "limegreen",
+  "Deinococcus-Thermus" = "black",
+  Deltaproteobacteria = "grey", 
+  Firmicutes = "#3E9B96",
+  Gammaproteobacteria = "cyan",
+  Gemmatimonadetes = "yellow",
+  Gracilibacteria = "#FD823F",
+  JTB23 = "#B5D6AA",
+  Latescibacteria = "salmon4",
+  Lentisphaerae = "palevioletred1",
+  Nitrospirae = "forestgreen",
+  Omnitrophica = "violet",
+  Microgenomates = "blue",
+  Parcubacteria = "#531A4D",
+  Planctomycetes = "darkorange", 
+  Proteobacteria_unclassified = "greenyellow",
+  Spirochaetae = "royalblue",
+  TA06 = "peachpuff",
+  TA18 = "burlywood", 
+  TM6 = "olivedrab",
+  Verrucomicrobia = "purple4",
+  "WCHB1-60" = "green")
+
+# Set global colors for the filter fraction sizes 
+fraction_colors <- c(
+  Particle = "firebrick3", 
+  Free = "cornflowerblue",
+  Whole = "#FF6600", 
+  Sediment = "#8A2667")
+
+# Set global colors for the depth of the sample
+depth_colors <- c(
+  Top = "#1AB58A",
+  Bottom = "#2F438A")
+
+# Set global colors for the lakesite (i.e. sampling station)
+lakesite_colors <- c(
+  MIN = "#C24704",
+  MBR = "#D9CC3C",
+  MDP = "#A0E0BA",
+  MOT = "#00ADA7")
+
+# Set global colors for the season
+season_colors <- c(
+  Spring = "#675B78",
+  Summer = "#AC6C82",
+  Fall = "#EB7B88")
+
+# Set global colors for the year
+year_colors <- c(
+  "2014" = "#F8BB0A", 
+  "2015" = "#FF4E50")
